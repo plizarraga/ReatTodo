31f7b574b4859f276f468a0c3a0c36378deb29db
Pedro Lizarraga <plizarraga@fender.com>
2017-10-02 16:04:15 -0700

    Add status todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,23 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {id: 1, text: 'React JS Course'},
+                {id: 2, text: 'Angular JS Course'},
+                {id: 3, text: 'Vue JS Course'},
+                {id: 4, text: 'Node JS Course'}
+            ]
+        }
+    },
     render: function () {
+        var {todos} = this.state;
+
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         )
     }
